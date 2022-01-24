3de9b10db79e8b06b46768e5360d76ee36e9808f
blentell <78812236+blentell@users.noreply.github.com>
2022-01-24 17:40:53 -0500

    Update q7-q11.js

--- q7-q11.js
+++ q7-q11.js
@@ -2,10 +2,20 @@ const x = 6
 
 // 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
 
+function addNumbers(input1, input2) {
+  return input1 + input2 + x;
+}
+console.log(addNumbers(1, 2));
+
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
+
+const addNumbersAgain = (input1, input2) => input1 + input2 + x;
+console.log(addNumbersAgain(1, 2))
 
 // 9. Write a function that returns another function. (use arrow functions please)
 
+const returnNumbers = () => addNumbersAgain;
+console.log(returnNumbers()(1, 2));
 
 // 10. given the following code:
 
@@ -22,6 +32,7 @@ console.log(getFunction()(2))
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
 
+// insideFunc is within closure of getFunction so will have access to y anywhere it gets called, whether within the same file or any file it may be exported to.
 
 
 // 11. write a function that takes "couldThrowError()" as a callback argument.
@@ -30,11 +41,21 @@ console.log(getFunction()(2))
 
 const couldThrowError = () => {
   
-  if(Math.ceil(Math.random() * 2) < 2){
-    throw new Error("Error was thrown");
+  if (Math.ceil(Math.random() * 2) < 2) {
+    throw new Error ("Error was thrown");
   }
   
   return 'success'
 }
+
+try {
+  let result = couldThrowError()
+  let value = couldThrowError(result)
+} catch (e) {
+  value = 'less than 2'
+  console.error(e.message, value);
+}
+
+
 
 
